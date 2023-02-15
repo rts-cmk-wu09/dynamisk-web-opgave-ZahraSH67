f2fa6ab90d3d5ff0fb4d7d8f41bf0866830b8ba5
Zahra <zahra.shahabi.dk@gmail.com>
2023-02-15 12:41:53 +0100

    Add css and js of Hero to project

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    
+    //HERO
+    const heroSection = document.querySelector(".hero")
+    
+    const heroImage = document.createElement("img")
+    heroImage.src = hero.image
+    heroSection.appendChild(heroImage)
+    heroImage.classList.add("heroImage")
+
+    const introduction = document.createElement("div")
+    heroSection.appendChild(introduction)
+    introduction.classList.add("introduction")
+
+    const headlineArr  = hero.headline.split(" ")
+    console.log(headlineArr)
+     const s1 = headlineArr.slice(0, headlineArr.indexOf("can")).join(" ")
+     const s2 = headlineArr.slice(headlineArr.indexOf("save"), headlineArr.indexOf("in")).join(" ")
+     const s3 = headlineArr.slice(headlineArr.indexOf("in"),headlineArr.length).join(" ")
+     console.log(s1)
+
+    const openSpan = '<span class ="span1" >'
+    const closeSpan = "</span>"
+
+    const headlineWithSpan = s1 + " " + openSpan + s2 + closeSpan + " " + s3
+    introduction.innerHTML = `<h2 class="headlineWithSpan"> ${headlineWithSpan} </h2>`
+    
+    const headParaf = document.createElement("p")
+     introduction.appendChild(headParaf)
+     headParaf.classList.add("headParaf")
+     headParaf.textContent = hero.copy
+
+     const exploreBTN = document.createElement("button")
+     introduction.appendChild(exploreBTN)
+     exploreBTN.classList.add("exploreBTN")
+     exploreBTN.innerHTML = `<img src="${hero.icon}">`
+     exploreBTN.innerHTML += '<p>Explore</p>'
+
     // slå dig løs her... 
 
     //eksempel på at udskrive alle overskrifter i services i konsollen:
